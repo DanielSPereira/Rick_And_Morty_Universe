20603936f69cefbf65fec4f5f000531f7ef7bd6d
DanielSPereira <danielsampaiopereira@outlook.com>
2022-05-26 22:38:59 -0300

    feat: CharacterInfoModal implemented as component

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -4,7 +4,7 @@ export function CharacterCard({ handleOpenModal }: { handleOpenModal: () => void
     return (
         <div className="card box-shadow">
             <img src="https://images5.alphacoders.com/796/796108.jpg" alt="" />
-            <div className="w-full h-full p-4">
+            <div className="w-full p-4">
                 <h1 className="character-name">Summer Smith</h1>
 
                 <span className="block">
@@ -12,22 +12,22 @@ export function CharacterCard({ handleOpenModal }: { handleOpenModal: () => void
                     <p className="status-specie">Alive - Human</p>
                 </span>
                 
-                <span className="block mt-3">
+                {/* <span className="block mt-3">
                     <span className="sub-title">Last known location:</span>
                     <p className="answer">Earth &#40;Replacement Dimension&#41;</p>
-                </span>
+                </span> */}
 
-                <span className="block mt-3">
+                {/* <span className="block mt-3">
                     <span className="sub-title">First seen in:</span>
                     <p className="answer">Mortynight Run</p>
-                </span>
+                </span> */}
 
                 <button 
                     type="button" 
                     className="view-details" 
                     onClick={handleOpenModal}
                 >
-                    View Details
+                    More Details
                 </button>
             </div>
         </div>
--- src/components/CharacterInfoModal/index.tsx
+++ src/components/CharacterInfoModal/index.tsx
@@ -14,6 +14,35 @@ export function CharacterInfoModal({ isModalOpen, handleCloseModal }: ICharacter
             className="modal"
             overlayClassName="overlay-modal"
         >
+            <img src="https://images5.alphacoders.com/796/796108.jpg" alt="" />
+            <div className="w-full py-4 px-8">
+                <h1 className="character-name">Summer Smith</h1>
+
+                <span className="block">
+                    <span className="status-circle"></span>
+                    <p className="status-specie">Alive - Human</p>
+                </span>
+                
+                <span className="block mt-3">
+                    <span className="sub-title">Last known location:</span>
+                    <p className="answer">Earth &#40;Replacement Dimension&#41;</p>
+                </span>
+
+                <span className="block mt-3">
+                    <span className="sub-title">First seen in:</span>
+                    <p className="answer">Mortynight Run</p>
+                </span>
+
+                <span className="block mt-3">
+                    <span className="sub-title">Episodes Amount:</span>
+                    <p className="answer">5</p>
+                </span>
+
+                <span className="block mt-3">
+                    <span className="sub-title">Creation Date:</span>
+                    <p className="answer">02/12/2018</p>
+                </span>
+        </div>
 
         </Modal>
     )
